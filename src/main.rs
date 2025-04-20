8389be73e523b9fa43807705c00a568369c0ef00
Adam Piziak <adpiziak@gmail.com>
2025-04-20 13:58:27 -0400

    dev

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::time::Duration;
 
 use bevy::{
-    color::palettes::css::WHITE,
+    color::palettes::css::{FOREST_GREEN, WHITE},
     dev_tools::fps_overlay::{FpsOverlayConfig, FpsOverlayPlugin},
     pbr::{
         CascadeShadowConfigBuilder, ExtendedMaterial, MaterialExtension, NotShadowCaster,
-        wireframe::WireframeConfig,
+        NotShadowReceiver, wireframe::WireframeConfig,
     },
     prelude::*,
     render::{
@@ -13,17 +13,19 @@ use bevy::{
         extract_component::ExtractComponent,
         extract_resource::{ExtractResource, ExtractResourcePlugin},
         gpu_readback::{Readback, ReadbackComplete},
-        mesh::VertexAttributeValues,
+        mesh::{ConeMeshBuilder, VertexAttributeValues},
         render_asset::RenderAssets,
         render_graph::{self, RenderGraph, RenderLabel},
         render_resource::{binding_types::storage_buffer, *},
         renderer::{RenderContext, RenderDevice},
         settings::{RenderCreation, WgpuSettings},
         storage::{GpuShaderStorageBuffer, ShaderStorageBuffer},
+        view::VisibilityRange,
     },
     text::FontSmoothing,
 };
 use lod::{CdlodMaterials, EnableWireframe, move_mock_camera, render_lod, setup_mock_camera};
+use rand::random_range;
 
 const COMPUTE_SHADER_ASSET_PATH: &str = "compute.wgsl";
 const TERRAIN_SHADER_PATH: &str = "terrain.wgsl";
@@ -292,9 +294,78 @@ struct TerrainState {
     heightmap: Vec<f32>,
 }
 
-fn render_terrain(mut compute_finished: EventReader<ComputeFinished>) {
-    for ev in compute_finished.read() {
-        println!("COMPUT FINISHED");
+fn render_terrain(
+    mut compute_finished: EventReader<ComputeFinished>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    terrain_state: Res<TerrainState>,
+    mut commands: Commands,
+    query: Query<(Entity, &BoxLabel2)>,
+
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    if !compute_finished.is_empty() {
+        let rand_offset: f32 = 600.0;
+        for (entity, label) in query.iter() {
+            commands.entity(entity).despawn();
+        }
+        // for (entity, label) in box_query.iter() {
+        //     ecommands.entity(entity).despawn();
+        // }
+
+        // let tree =
+        //     asset_server.load(GltfAssetLabel::Scene(0).from_asset("tree/scene.gltf"));
+        // let box_mesh = meshes.add(Cuboid::from_size(Vec3::splat(2.0)));
+        let box_mesh = meshes.add(ConeMeshBuilder::new(0.8, 1.5, 4).build());
+        let mut box_mat: StandardMaterial = Color::from(FOREST_GREEN).darker(0.16).into();
+        box_mat.perceptual_roughness = 1.0;
+        let box_mat = materials.add(box_mat);
+        let data = &terrain_state.heightmap;
+        for _ in 0..20 {
+            // let offset_x = random_range(-rand_offset..rand_offset);
+            let offset_x = random_range(0_f32..rand_offset);
+            // let offset_y = random_range(-rand_offset..rand_offset);
+            // let offset_z = random_range(-rand_offset..rand_offset);
+            let offset_z = random_range(0_f32..rand_offset);
+
+            // buffer.ob
+            //
+
+            let i = offset_z.round() as usize * MAP_HEIGHT + offset_x as usize;
+            let i = i.min(data.len() - 1);
+            let height = data[i];
+            println!("{height}");
+            if height < 1.0 {
+                let hlod_co = 400.0;
+                commands.spawn((
+                    // SceneRoot(tree.clone_weak()),
+                    Mesh3d(box_mesh.clone()),
+                    MeshMaterial3d(box_mat.clone()),
+                    BoxLabel2,
+                    NotShadowReceiver,
+                    // NotShadowCaster,
+                    VisibilityRange::abrupt(30.0, hlod_co),
+                    // NotShadowCaster,
+                    // Transform::from_xyz(0.0, 0.0, 0.0),
+                    Transform::from_xyz(offset_x - 300.0, height, offset_z - 300.0),
+                    // .with_scale(Vec3::splat(0.5)),
+                ));
+                commands.spawn((
+                    // SceneRoot(tree.clone_weak()),
+                    Mesh3d(box_mesh.clone()),
+                    MeshMaterial3d(box_mat.clone()),
+                    BoxLabel2,
+                    NotShadowReceiver,
+                    NotShadowCaster,
+                    VisibilityRange::abrupt(hlod_co, 700.0),
+                    // NotShadowCaster,
+                    // Transform::from_xyz(0.0, 0.0, 0.0),
+                    Transform::from_xyz(offset_x - 300.0, height, offset_z - 300.0),
+                    // .with_scale(Vec3::splat(0.5)),
+                ));
+            }
+            println!("COMPUT FINISHED");
+        }
+        compute_finished.clear();
     }
 }
 
